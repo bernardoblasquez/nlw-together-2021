876f15790000057189866f71da682180f3cbcfed
Bernardo <bernardo.bt@gmail.com>
2021-06-21 22:45:20 -0300

    feat: criação das páginas/componentes Home e NewRoom

---
+++ letmeask/src/Pages/Home.tsx
@@ -0,0 +1,44 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+import { Button } from '../Components/Button'
+import '../styles/auth.scss'
+
+export  function Home(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência dem tempo-real</p>
+            </aside>
+            
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="letmask" />
+                    
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="logo do google" />
+                        Crie sua sala com o google
+                    </button>
+
+                    <div className="separator">
+                        ou entre em uma sala
+                    </div>
+
+                    <form action="">
+                        <input 
+                            type="text" 
+                            placeholder="Digite o código da sala" />
+                        <Button>
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+
+        </div>
+        
+    )
+} 
---
+++ letmeask/src/Pages/NewRoom.tsx
@@ -0,0 +1,41 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+import { Button } from '../Components/Button'
+import '../styles/auth.scss'
+
+export  function NewRoom(){
+    return(
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência dem tempo-real</p>
+            </aside>
+            
+
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="letmask" />
+                   
+                    <h2>Criar uma nova sala</h2>
+
+                    <form action="">
+                        <input 
+                            type="text" 
+                            placeholder="Nome da sala" />
+                        <Button>
+                            Criar na sala
+                        </Button>
+                    </form>
+
+                    <p>
+                        Quer entrar em uma nova sala <a href="">clique aqui</a>
+                    </p>
+                </div>
+            </main>
+
+        </div>
+        
+    )
+} 
