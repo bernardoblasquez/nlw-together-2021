8f1dd39ce4d4aae0a779c09a18b0c7a57017de42
Bernardo <bernardo.bt@gmail.com>
2021-06-23 22:38:51 -0300

    feat: criação do componente Room

--- letmeask/src/Pages/Rooms.tsx
+++ letmeask/src/Pages/Rooms.tsx
@@ -1,7 +1,38 @@
+import logoImg from '../assets/images/logo.svg'
+import { Button } from '../Components/Button';
 
+import '../styles/room.scss'
 
 export function Room(){
     return(
-        <h1>Nova Sala</h1>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main className="content">
+                
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 Perguntas</span>
+                </div>
+
+                <form action="">
+                    <textarea placeholder="O que vc quer perguntar?" />
+                    
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta <button>faça seu login</button>.</span>
+                        <Button type="submit">Enviar pergunta</Button>
+                    </div>
+                    
+                </form>
+            </main>
+
+        </div>
+
+        
     );
 }
