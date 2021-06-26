7ce6bb242ca4d20787169a7f8ad61f50b3017175
Bernardo <bernardo.bt@gmail.com>
2021-06-26 17:05:17 -0300

    feat: rota para admin do site

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './Pages/Home';
 import { NewRoom } from './Pages/NewRoom';
 import { Room } from './Pages/Rooms';
 import { AuthContextProvider } from './Context/AuthContext'; 
+import { AdminRoom } from './Pages/AdminRoom';
 
 
 
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" exact component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
