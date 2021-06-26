21e4f601792617d59fee633de63237845b4fe938
Bernardo <bernardo.bt@gmail.com>
2021-06-26 16:53:06 -0300

    feat: refatoração de código - função passada para useRoom e criação da lógica e do botão de like.

--- letmeask/src/Pages/Rooms.tsx
+++ letmeask/src/Pages/Rooms.tsx
@@ -1,35 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router'; 
 import { FormEvent } from 'react';
-import logoImg from '../assets/images/logo.svg'
 import { Button } from '../Components/Button';
 import { RoomCode } from '../Components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../Services/firebase';
-
-import '../styles/room.scss';
 import { Questions } from '../Components/Questions';
+import { useRoom } from '../hooks/useRoom';
 
-type FirebaseQuestions = Record<string, {
-    content: string,
-    author:{
-        name: string,
-        avatar: string,
-    },
-    isHighLighted: string,
-    isAnswered: string
-} >
+import logoImg from '../assets/images/logo.svg'
+import '../styles/room.scss';
 
-type Question = {
-    id: string;
-    content: string,
-    author:{
-        name: string,
-        avatar: string,
-    },
-    isHighLighted: string,
-    isAnswered: string
-}
+
+
+
+
 
 type RoomParams = {
     id: string
@@ -39,35 +24,13 @@ export function Room(){
 
     const { user } = useAuth()
     const [newQuestion, setNewQuestion] = useState('');
-    const [questions, setQuestions] = useState<Question[]>([]);
-    const [title, setTitle] = useState('')
+    
     const roomId = useParams<RoomParams>();
 
-    useEffect(()=>{
-        const roomRef = database.ref(`rooms/${roomId.id}`)
+    const {questions, title} = useRoom(roomId.id)
+   
 
-        roomRef.on('value', room => {
-            const databaseRoom = room.val();
-            const firebaseQuestions = databaseRoom.questions as FirebaseQuestions ?? {}
-            const parsedQuestions  = Object.entries(firebaseQuestions).map(([key, value]) => {
-                return(
-                    {
-                        id: key,
-                        content: value.content,
-                        author: value.author,
-                        isHighLighted: value.isHighLighted,
-                        isAnswered: value.isAnswered
-                    }
-                )
-            })
-            setTitle(databaseRoom.title)
-            setQuestions(parsedQuestions)
-
-        })
-
-    },[])
-
-    async function handleSendQuastion(event: FormEvent) {
+    async function handleSendQuestion(event: FormEvent) {
         
         event.preventDefault();
 
@@ -94,12 +57,26 @@ export function Room(){
         setNewQuestion('')
     }
 
+    async function handleLikeQuestion(questionId: string, likeId:string|undefined) {
+
+        if(likeId){
+            await database.ref(`rooms/${roomId.id}/questions/${questionId}/likes/${likeId}`).remove()
+        }else{
+            const newLike = await database.ref(`rooms/${roomId.id}/questions/${questionId}/likes`).push({
+                authorId: user?.id,
+            })
+        }
+
+        
+    }
+
     return(
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
                     <RoomCode code={roomId.id}/>
+                    
                 </div>
             </header>
 
@@ -110,7 +87,7 @@ export function Room(){
                     {questions.length > 0 && <span> {questions.length} perguntas</span>}
                 </div>
 
-                <form action="" onSubmit={handleSendQuastion}>
+                <form action="" onSubmit={handleSendQuestion}>
                     <textarea 
                         placeholder="O que vc quer perguntar?" 
                         onChange={event => setNewQuestion(event.target.value)}
@@ -138,8 +115,22 @@ export function Room(){
                             <Questions 
                                 key={question.id}
                                 content={question.content}
-                                author={question.author}
-                            />
+                                author={question.author}>
+                                
+                                <button
+                                    className={`like-button ${question.likeId ? 'liked': ''}`}
+                                    type='button'
+                                    aria-label="Marcar como gostei"
+                                    onClick={() => handleLikeQuestion(question.id, question.likeId)}
+                                >                    
+                                    
+                                    {question.likeCount > 0 && <span>{question.likeCount}</span>}
+                                   
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </button>
+                            </Questions>
                         )
                     })}
                 </div>
